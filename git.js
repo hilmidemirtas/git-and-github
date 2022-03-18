0a0631334b10e2ff2fe2726e185947849bdf4e8c
hilmi.demirtas <hilmidemirtas94@gmail.com>
2022-03-18 12:28:20 +0300

    deneme yaptım

---
+++ git.js
@@ -0,0 +1,9 @@
+console.log("git kullanımı");
+console.log("selam")
+console.log("hi")
+
+console.log("another try")
+/* 
+this is a comment */
+
+git diff 7783ce107aa77cdf3e7b5c8521e1e96dd217d7c5 .. c45995a78c264c8ede88487c1bd87195af7ffb27 git.js
